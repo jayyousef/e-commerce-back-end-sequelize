408570707160d08423df354fb672095a260a054e
Jay Yousef <jay.yousef@gmail.com>
2021-04-27 08:40:31 -0500

    fixed bug that created an extra column. currently have all post and get requests working properly and site is ready to be deployed

--- models/index.js
+++ models/index.js
@@ -9,9 +9,9 @@ Product.belongsTo(Category, {foreignKey: 'category_id'})
 // Categories have many Products
 Category.hasMany(Product, {foreignKey: 'category_id'})
 // Products belongToMany Tags (through ProductTag)
-Product.belongsToMany(Tag,{as: 'product_id', through:ProductTag, foreignKey:'product_id',constraints: false})
+Product.belongsToMany(Tag,{through:ProductTag, foreignKey:'product_id',constraints: false})
 // Tags belongToMany Products (through ProductTag)
-Tag.belongsToMany(Tag,{as: 'tag_id', through:ProductTag, foreignKey:'tag_id',constraints: false})
+Tag.belongsToMany(Product,{through:ProductTag, foreignKey:'tag_id',constraints: false})
 
 module.exports = {
   Product,
